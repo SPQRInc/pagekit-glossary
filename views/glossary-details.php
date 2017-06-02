20bf62f11f7d5965c5b6405f160eabfa1217e711
Roman <roman.lossin@icloud.com>
2017-06-02 14:50:54 +0200

    ADD: Adjustable style of the heading in the item's detail page

--- views/glossary-details.php
+++ views/glossary-details.php
@@ -2,13 +2,13 @@
 <div id="glossary-details">
 	<div class="uk-grid" data-uk-grid-margin>
 		<div class="uk-width-1-1">
-			<<?= $config->heading_style ?> class="uk-heading-large"><?= $item->title ?></<?= $config->heading_style ?> >
-			<div class="uk-flex uk-flex-wrap uk-margin" data-uk-margin="">
-				<?php foreach($item->marker as $tag) : ?>
-					<div class="uk-badge uk-margin-small-right"><?= $tag ?></div>
-				<?php endforeach; ?>
-			</div>
-			<p><?= $item->content ?></p>
+			<<?=$heading ?> class="uk-heading-large"><?= $item->title ?></<?=$heading?> >
+		<div class="uk-flex uk-flex-wrap uk-margin" data-uk-margin="">
+			<?php foreach($item->marker as $tag) : ?>
+				<div class="uk-badge uk-margin-small-right"><?= $tag ?></div>
+			<?php endforeach; ?>
 		</div>
+		<p><?= $item->content ?></p>
 	</div>
+</div>
 </div>
