5286272b661c1d65f89db720d707b89e65a7e041
Roman <roman.lossin@icloud.com>
2017-06-12 08:27:03 +0200

    - FIX: Checks if content is available and prevents errors e.g. on blog's overview

--- src/Plugin/GlossaryPlugin.php
+++ src/Plugin/GlossaryPlugin.php
@@ -17,68 +17,71 @@ class GlossaryPlugin implements EventSubscriberInterface
 	 */
 	public function onContentPlugins( ContentEvent $event )
 	{
-		libxml_use_internal_errors(true);
+		libxml_use_internal_errors( true );
 		
 		$node   = App::node();
 		$config = App::module( 'glossary' )->config();
 		
 		$content = $event->getContent();
-		$query   = Item::where( [ 'status = ?' ], [ Item::STATUS_PUBLISHED ] );
 		
-		$dom = new \DOMDocument();
-		$dom->loadHtml( $content );
-		$xpath = new \DOMXPath( $dom );
-		
-		$target    = $config[ 'target' ];
-		$tooltip   = $config[ 'show_tooltip' ];
-		$class     = $config[ 'hrefclass' ];
-		$hrefclass = ( $class ? "class='$class'" : "" );
-		
-		if ( $node->link != "@glossary" ) {
+		if ( $content ) {
+			$query = Item::where( [ 'status = ?' ], [ Item::STATUS_PUBLISHED ] );
 			
-			$markers = [];
+			$dom = new \DOMDocument();
+			$dom->loadHtml( $content );
+			$xpath = new \DOMXPath( $dom );
 			
-			foreach ( $items = $query->get() as $key => $item ) {
+			$target    = $config[ 'target' ];
+			$tooltip   = $config[ 'show_tooltip' ];
+			$class     = $config[ 'hrefclass' ];
+			$hrefclass = ( $class ? "class='$class'" : "" );
+			
+			if ( $node->link != "@glossary" ) {
 				
-				if ( $item->get( 'markdown' ) ) {
-					$item->content = App::markdown()->parse( $item->content );
-					$item->excerpt = App::markdown()->parse( $item->excerpt );
-				}
+				$markers = [];
 				
-				if ( empty( $item->excerpt ) ) {
-					$item->excerpt = $item->content;
-				}
-				
-				$url                                   = App::url( '@glossary/id', [ 'id' => $item->id ], 'base' );
-				$markers[ strtolower( $item->title ) ] =
-					[ 'text' => $item->title, 'url' => $url, 'excerpt' => $item->excerpt ];
-				
-				if ( is_array( $item->marker ) && !empty ( $item->marker ) ) {
-					foreach ( $item->marker as $marker ) {
-						$markers[ strtolower( $marker ) ] =
-							[ 'text' => $marker, 'url' => $url, 'excerpt' => $item->excerpt ];
+				foreach ( $items = $query->get() as $key => $item ) {
+					
+					if ( $item->get( 'markdown' ) ) {
+						$item->content = App::markdown()->parse( $item->content );
+						$item->excerpt = App::markdown()->parse( $item->excerpt );
+					}
+					
+					if ( empty( $item->excerpt ) ) {
+						$item->excerpt = $item->content;
+					}
+					
+					$url                                   = App::url( '@glossary/id', [ 'id' => $item->id ], 'base' );
+					$markers[ strtolower( $item->title ) ] =
+						[ 'text' => $item->title, 'url' => $url, 'excerpt' => $item->excerpt ];
+					
+					if ( is_array( $item->marker ) && !empty ( $item->marker ) ) {
+						foreach ( $item->marker as $marker ) {
+							$markers[ strtolower( $marker ) ] =
+								[ 'text' => $marker, 'url' => $url, 'excerpt' => $item->excerpt ];
+						}
 					}
 				}
-			}
-			
-			foreach ( $markers as $marker ) {
-				foreach ( $xpath->query( '//text()[not(ancestor::a)]' ) as $node ) {
-					$text     = $marker[ 'text' ];
-					$url      = $marker[ 'url' ];
-					$tip      = strip_tags( $marker[ 'excerpt' ] );
-					$tooltip  = ( $tooltip ? "data-uk-tooltip='' title='$tip'" : "" );
-					$replaced = preg_replace(
-						'/\b' . preg_quote( $text, "/" ) . '\b/i',
-						"<a href='$url' $hrefclass target='$target' $tooltip>\$0</a>",
-						$node->wholeText
-					);
-					$newNode  = $dom->createDocumentFragment();
-					$newNode->appendXML( $replaced );
-					$node->parentNode->replaceChild( $newNode, $node );
+				
+				foreach ( $markers as $marker ) {
+					foreach ( $xpath->query( '//text()[not(ancestor::a)]' ) as $node ) {
+						$text     = $marker[ 'text' ];
+						$url      = $marker[ 'url' ];
+						$tip      = strip_tags( $marker[ 'excerpt' ] );
+						$tooltip  = ( $tooltip ? "data-uk-tooltip='' title='$tip'" : "" );
+						$replaced = preg_replace(
+							'/\b' . preg_quote( $text, "/" ) . '\b/i',
+							"<a href='$url' $hrefclass target='$target' $tooltip>\$0</a>",
+							$node->wholeText
+						);
+						$newNode  = $dom->createDocumentFragment();
+						$newNode->appendXML( $replaced );
+						$node->parentNode->replaceChild( $newNode, $node );
+					}
 				}
+				
+				$event->setContent( utf8_decode( $dom->saveHTML( $dom->documentElement ) ) );
 			}
-			
-			$event->setContent(utf8_decode($dom->saveHTML($dom->documentElement)));
 		}
 	}
 	
